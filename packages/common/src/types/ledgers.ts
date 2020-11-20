ff1184f374337703061853b0626eb95e76f0a32c
Dan Burton <danburton.email@gmail.com>
2020-11-20 17:17:09 -0500

    wip2

--- packages/common/src/types/ledgers.ts
+++ packages/common/src/types/ledgers.ts
@@ -1,5 +1,6 @@
 export function getSupportedLedgers(): Array<object>{
     // Need to add access to additional ledger types from import
     return [{"name": "mainnet", "genesisId": 'mainnet-v1.0', "genesisHash": ""}, 
-            {"name": "testnet", "genesisId": 'testnet-v1.0', "genesisHash": ""}];
+            {"name": "testnet", "genesisId": 'testnet-v1.0', "genesisHash": ""},
+            {"name": "localhost", "genesisId": 'devnet-v1', "genesisHash": "5o9fo9Y65tWHXiS+0d73UXOB3D+eM5zI6sJ+CX9kqmc="}];
 }
